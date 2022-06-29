0399a1ad9daa00fed7a5204cc8511d8801c175bf
Yashveer soni <94055755+Yashveersoni123@users.noreply.github.com>
2022-06-29 19:19:57 +0530

    Add files via upload

---
+++ simple.js
@@ -0,0 +1,14 @@
+//cpu random
+const computer=['stone','paper', 'scissor'];
+const random=Math.floor(Math.random() * 3);
+console.log(computer[random]);
+//user random
+addEventListener("click",function()
+{  
+    var a= document.getElementsByClassName("btn1");
+   var b="stone";
+    console.log(b);
+
+})
+
+
