50a61bed709680ee603272907fea7b21659010af
Yashveer soni <94055755+Yashveersoni123@users.noreply.github.com>
2022-06-30 23:19:13 +0530

    Update simple.js

--- simple.js
+++ simple.js
@@ -1,14 +1,64 @@
-//cpu random
-const computer=['stone','paper', 'scissor'];
-const random=Math.floor(Math.random() * 3);
-console.log(computer[random]);
-//user random
-addEventListener("click",function()
-{  
-    var a= document.getElementsByClassName("btn1");
-   var b="stone";
-    console.log(b);
+//user
+let i;
+function function1()
 
+{
+    const img1=document.querySelector('#null1');
+    img1.src="stone.jpg";
+}
+function function2()
+{
+     const img1=document.querySelector('#null1');
+    img1.src="paper.jpg";
+  
+}
+function function3()
+{
+ const img1=document.querySelector('#null1');
+    img1.src="scissor.jpg";
+   
+}
+// cpu random
+
+const computer=['stone','paper', 'scissor'];
+var random=Math.floor(Math.random() * 3);
+console.log(random);
+function function5()
+{
+    const img2=document.querySelector("#null");
+    if(random==1)
+    {
+        img2.src="stone.jpg";
+    }
+
+}
+function5();
+    //  const img2=document.querySelector('#null');
+    //  img2.src="stone.jpg";
+    
+
+// if(random==1)
+// {
+//      const img2=document.querySelector('#null');
+//      img2.src="paper.jpg";
+    
+// }
+// if(random==2)
+// {
+//      const img2=document.querySelector('#null');
+//      img2.src="scissor.jpg";
+    
+// }
+
+//music
+const music = new Audio('drank water.mp3');
+music.loop =false;
+music.playbackRate = 1;
+
+a=addEventListener('click' ,function()
+{
+ console.log("access");
+ music.play();
 })
 
 
